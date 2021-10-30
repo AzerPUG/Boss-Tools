c6282f9b9906f1a065db3704426065c698b54364
Arco Dielhof <arcodielhof@hotmail.com>
2021-10-30 21:41:33 +0200

    Dormazain Functionality and removed RohKalo functionality to make it generic in to BossTools.lua.

--- BossTools.lua
+++ BossTools.lua
@@ -4,3 +4,181 @@ if AZP.VersionControl == nil then AZP.VersionControl = {} end
 AZP.VersionControl["BossTools"] = 8
 if AZP.BossTools == nil then AZP.BossTools = {} end
 if AZP.BossTools.Events == nil then AZP.BossTools.Events = {} end
+
+AZP.BossTools.ParentOptionFrame = nil
+AZP.BossTools.PopUpFrame = nil
+AZP.BossTools.BossFrames = {}
+
+local AZPRTBossToolsFrame = nil
+
+local soundID = 8959
+local soundChannel = 1
+
+local BossFileIDs =
+{
+    Tarragrue = GetFileIDFromPath("Interface\\ENCOUNTERJOURNAL\\UI-EJ-BOSS-Tarragrue.blp"),
+    Dormazain = GetFileIDFromPath("Interface\\ENCOUNTERJOURNAL\\UI-EJ-BOSS-Soulrender Dormazain.blp"),
+    RohKalo   = GetFileIDFromPath("Interface\\ENCOUNTERJOURNAL\\UI-EJ-BOSS-Fatescribe Roh-Talo.blp"),
+}
+
+function AZP.BossTools.OnLoad()
+    AZP.BossTools.ParentOptionFrame = CreateFrame("FRAME", nil)
+    AZP.BossTools.ParentOptionFrame.name = "|cFF00FFFFAzerPUG's BossTools|r"
+    InterfaceOptions_AddCategory(AZP.BossTools.ParentOptionFrame)
+
+    AZP.BossTools.ParentOptionFrame.Header = AZP.BossTools.ParentOptionFrame:CreateFontString("AZPBossToolsRohKaloOptionPanel", "ARTWORK", "GameFontNormalHuge")
+    AZP.BossTools.ParentOptionFrame.Header:SetPoint("TOP", 0, -10)
+    AZP.BossTools.ParentOptionFrame.Header:SetText(string.format("AzerPUG's BossTools v%s", AZP.VersionControl["BossTools"]))
+
+    AZP.BossTools.ParentOptionFrame.SubHeader = AZP.BossTools.ParentOptionFrame:CreateFontString("AZPBossToolsRohKaloOptionPanel", "ARTWORK", "GameFontNormalLarge")
+    AZP.BossTools.ParentOptionFrame.SubHeader:SetPoint("TOP", AZP.BossTools.ParentOptionFrame.Header, "BOTTOM", 0, -5)
+    AZP.BossTools.ParentOptionFrame.SubHeader:SetText("Options Panel")
+
+    AZP.BossTools:CreateSelectorFrame()
+    AZP.BossTools:CreatePopUpFrame()
+end
+
+function AZP.BossTools:CreateSelectorFrame()
+    AZPRTBossToolsFrame = CreateFrame("FRAME", nil, UIParent, "BackdropTemplate")
+    AZPRTBossToolsFrame:SetPoint("CENTER", 0, 0)
+    AZPRTBossToolsFrame:SetSize(265, 150)
+    AZPRTBossToolsFrame:SetBackdrop({
+        bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+        edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+        edgeSize = 10,
+        insets = {left = 3, right = 3, top = 3, bottom = 3},
+    })
+
+    AZPRTBossToolsFrame.Header = AZPRTBossToolsFrame:CreateFontString("AZPRTBossToolsFrame", "ARTWORK", "GameFontNormalHuge")
+    AZPRTBossToolsFrame.Header:SetSize(AZPRTBossToolsFrame:GetWidth(), 25)
+    AZPRTBossToolsFrame.Header:SetPoint("TOP", 0, -5)
+    AZPRTBossToolsFrame.Header:SetText(string.format("AzerPUG's BossTools v%s", AZP.VersionControl["BossTools"]))
+
+    AZPRTBossToolsFrame.SubHeader = AZPRTBossToolsFrame:CreateFontString("AZPRTBossToolsFrame", "ARTWORK", "GameFontNormalLarge")
+    AZPRTBossToolsFrame.SubHeader:SetSize(AZPRTBossToolsFrame:GetWidth(), 25)
+    AZPRTBossToolsFrame.SubHeader:SetPoint("TOP", AZPRTBossToolsFrame.Header, "BOTTOM", 0, 5)
+    AZPRTBossToolsFrame.SubHeader:SetText("Boss Selector Frame")
+
+    local BossWidth, BossHeight = 100, 75
+
+    AZPRTBossToolsFrame.Tarragrue = CreateFrame("FRAME", nil, AZPRTBossToolsFrame)
+    AZPRTBossToolsFrame.Tarragrue:SetSize(BossWidth, BossHeight)
+    AZPRTBossToolsFrame.Tarragrue:SetPoint("BOTTOM", -80, -1)
+    AZPRTBossToolsFrame.Tarragrue:SetScript("OnMouseDown", function()  end) -- AZPRTBossToolsFrame:Hide() AZP.BossTools.BossFrames.Tarragrue:Show()
+    AZPRTBossToolsFrame.Tarragrue.Button = AZPRTBossToolsFrame.Tarragrue:CreateTexture(nil, "ARTWORK")
+    AZPRTBossToolsFrame.Tarragrue.Button:SetSize(AZPRTBossToolsFrame.Tarragrue:GetWidth(), 55)
+    AZPRTBossToolsFrame.Tarragrue.Button:SetPoint("BOTTOM", 0, 0)
+    AZPRTBossToolsFrame.Tarragrue.Button:SetTexture(BossFileIDs.Tarragrue)
+    AZPRTBossToolsFrame.Tarragrue.Button:SetDesaturated(true)
+    AZPRTBossToolsFrame.Tarragrue.Label = AZPRTBossToolsFrame.Tarragrue:CreateFontString("AZPRTBossToolsFrame", "ARTWORK", "GameFontNormalLarge")
+    AZPRTBossToolsFrame.Tarragrue.Label:SetSize(AZPRTBossToolsFrame.Tarragrue:GetWidth() -20, 25)
+    AZPRTBossToolsFrame.Tarragrue.Label:SetPoint("TOP", -10, 0)
+    AZPRTBossToolsFrame.Tarragrue.Label:SetText("Tarragrue")
+
+    AZPRTBossToolsFrame.Tarragrue.ComingSoon = AZPRTBossToolsFrame.Tarragrue:CreateFontString("AZPRTBossToolsFrame", "ARTWORK", "GameFontNormalLarge")
+    AZPRTBossToolsFrame.Tarragrue.ComingSoon:SetSize(AZPRTBossToolsFrame.Tarragrue:GetWidth() -20, 50)
+    AZPRTBossToolsFrame.Tarragrue.ComingSoon:SetPoint("TOP", -10, -25)
+    AZPRTBossToolsFrame.Tarragrue.ComingSoon:SetText("Coming\nSoon!")
+
+    AZPRTBossToolsFrame.Dormazain = CreateFrame("FRAME", nil, AZPRTBossToolsFrame)
+    AZPRTBossToolsFrame.Dormazain:SetSize(BossWidth, BossHeight)
+    AZPRTBossToolsFrame.Dormazain:SetPoint("BOTTOM", 10, -1)
+    AZPRTBossToolsFrame.Dormazain:SetScript("OnMouseDown", function() AZPRTBossToolsFrame:Hide() AZP.BossTools.BossFrames.Dormazain:Show() end)
+    AZPRTBossToolsFrame.Dormazain.Button = AZPRTBossToolsFrame.Dormazain:CreateTexture(nil, "ARTWORK")
+    AZPRTBossToolsFrame.Dormazain.Button:SetSize(100, 55)
+    AZPRTBossToolsFrame.Dormazain.Button:SetPoint("BOTTOM", 0, 0)
+    AZPRTBossToolsFrame.Dormazain.Button:SetTexture(BossFileIDs.Dormazain)
+    AZPRTBossToolsFrame.Dormazain.Label = AZPRTBossToolsFrame.Dormazain:CreateFontString("AZPRTBossToolsFrame", "ARTWORK", "GameFontNormalLarge")
+    AZPRTBossToolsFrame.Dormazain.Label:SetSize(AZPRTBossToolsFrame.Dormazain:GetWidth() -20, 25)
+    AZPRTBossToolsFrame.Dormazain.Label:SetPoint("TOP", -10, 0)
+    AZPRTBossToolsFrame.Dormazain.Label:SetText("Dormazain")
+
+    AZPRTBossToolsFrame.RohKalo = CreateFrame("FRAME", nil, AZPRTBossToolsFrame)
+    AZPRTBossToolsFrame.RohKalo:SetSize(BossWidth, BossHeight)
+    AZPRTBossToolsFrame.RohKalo:SetPoint("BOTTOM", 100, -1)
+    AZPRTBossToolsFrame.RohKalo:SetScript("OnMouseDown", function() AZPRTBossToolsFrame:Hide() AZP.BossTools.BossFrames.RohKalo:Show() end)
+    AZPRTBossToolsFrame.RohKalo.Button = AZPRTBossToolsFrame.RohKalo:CreateTexture(nil, "ARTWORK")
+    AZPRTBossToolsFrame.RohKalo.Button:SetSize(100, 55)
+    AZPRTBossToolsFrame.RohKalo.Button:SetPoint("BOTTOM", 0, 0)
+    AZPRTBossToolsFrame.RohKalo.Button:SetTexture(BossFileIDs.RohKalo)
+    AZPRTBossToolsFrame.RohKalo.Label = AZPRTBossToolsFrame.RohKalo:CreateFontString("AZPRTBossToolsFrame", "ARTWORK", "GameFontNormalLarge")
+    AZPRTBossToolsFrame.RohKalo.Label:SetSize(AZPRTBossToolsFrame.RohKalo:GetWidth() -20, 25)
+    AZPRTBossToolsFrame.RohKalo.Label:SetPoint("TOP", -10, 0)
+    AZPRTBossToolsFrame.RohKalo.Label:SetText("Roh-Kalo")
+
+    AZPRTBossToolsFrame.closeButton = CreateFrame("Button", nil, AZPRTBossToolsFrame, "UIPanelCloseButton")
+    AZPRTBossToolsFrame.closeButton:SetSize(20, 21)
+    AZPRTBossToolsFrame.closeButton:SetPoint("TOPRIGHT", AZPRTBossToolsFrame, "TOPRIGHT", 2, 2)
+    AZPRTBossToolsFrame.closeButton:SetScript("OnClick", function() AZPRTBossToolsFrame:Hide() end)
+
+    --AZPRTBossToolsFrame:Hide()
+end
+
+function AZP.BossTools:CreatePopUpFrame()
+    AZP.BossTools.PopUpFrame = CreateFrame("FRAME", nil, UIParent)
+    AZP.BossTools.PopUpFrame:SetPoint("CENTER", 0, 250)
+    AZP.BossTools.PopUpFrame:SetSize(200, 50)
+
+    AZP.BossTools.PopUpFrame.text = AZP.BossTools.PopUpFrame:CreateFontString("PopUpFrame", "ARTWORK", "GameFontNormalHuge")
+    AZP.BossTools.PopUpFrame.text:SetPoint("CENTER", 0, 0)
+    AZP.BossTools.PopUpFrame.text:SetSize(AZP.BossTools.PopUpFrame:GetWidth(), AZP.BossTools.PopUpFrame:GetHeight())
+    AZP.BossTools.PopUpFrame.text:SetScale(0.5)
+    AZP.BossTools.PopUpFrame.text:Hide()
+end
+
+function AZP.BossTools:CheckIfDead(playerGUID)
+    local deathStatus
+    for i = 1, 40 do
+        local curGUID = UnitGUID("Raid" .. i)
+        if curGUID ~= nil then
+            if curGUID == playerGUID then
+                deathStatus = UnitIsDeadOrGhost("Raid" .. i)
+            end
+        end
+    end
+    return deathStatus
+end
+
+function AZP.BossTools:GetClassColor(classIndex)
+        if classIndex ==  0 then return 0.00, 0.00, 0.00      -- None
+    elseif classIndex ==  1 then return 0.78, 0.61, 0.43      -- Warrior
+    elseif classIndex ==  2 then return 0.96, 0.55, 0.73      -- Paladin
+    elseif classIndex ==  3 then return 0.67, 0.83, 0.45      -- Hunter
+    elseif classIndex ==  4 then return 1.00, 0.96, 0.41      -- Rogue
+    elseif classIndex ==  5 then return 1.00, 1.00, 1.00      -- Priest
+    elseif classIndex ==  6 then return 0.77, 0.12, 0.23      -- Death Knight
+    elseif classIndex ==  7 then return 0.00, 0.44, 0.87      -- Shaman
+    elseif classIndex ==  8 then return 0.25, 0.78, 0.92      -- Mage
+    elseif classIndex ==  9 then return 0.53, 0.53, 0.93      -- Warlock
+    elseif classIndex == 10 then return 0.00, 1.00, 0.60      -- Monk
+    elseif classIndex == 11 then return 1.00, 0.49, 0.04      -- Druid
+    elseif classIndex == 12 then return 0.64, 0.19, 0.79      -- Demon Hunter
+    end
+end
+
+function AZP.BossTools:WarnPlayer(text)
+    local curScale = 0.5
+    AZP.BossTools.PopUpFrame.text:SetScale(curScale)
+    AZP.BossTools.PopUpFrame.text:Show()
+    AZP.BossTools.PopUpFrame.text:SetText(text)
+    PlaySound(soundID, soundChannel)
+    C_Timer.NewTimer(2.5, function() AZP.BossTools.PopUpFrame.text:Hide() end)
+    C_Timer.NewTicker(0.005,
+    function()
+        curScale = curScale + 0.15
+        AZP.BossTools.PopUpFrame.text:SetScale(curScale)
+    end,
+    35)
+end
+
+AZP.BossTools.OnLoad()
+
+AZP.SlashCommands["BT"] = function()
+    if AZPRTBossToolsFrame ~= nil then AZPRTBossToolsFrame:Show() end
+end
+
+AZP.SlashCommands["bt"] = AZP.SlashCommands["BT"]
+AZP.SlashCommands["boos tools"] = AZP.SlashCommands["BT"]
+AZP.SlashCommands["Boss Tools"] = AZP.SlashCommands["BT"]
+AZP.SlashCommands["bosstools"] = AZP.SlashCommands["BT"]
+AZP.SlashCommands["BossTools"] = AZP.SlashCommands["BT"]
---
+++ Dormazain.lua
@@ -0,0 +1,351 @@
+if AZP == nil then AZP = {} end
+if AZP.VersionControl == nil then AZP.VersionControl = {} end
+
+if AZP.BossTools.Dormazain == nil then AZP.BossTools.Dormazain = {} end
+if AZP.BossTools.Dormazain.Events == nil then AZP.BossTools.Dormazain.Events = {} end
+
+local AssignedPlayers = {}
+local AZPBTDormazainOptions = nil
+local AZPBTDormazainGUIDs, AZPBTDormazainLeftEditBoxes, AZPBTDormazainMidEditBoxes, AZPBTDormazainRightEditBoxes = {}, {}, {}, {}
+
+local ChainsCount = 0
+
+local EventFrame = nil
+
+function AZP.BossTools.Dormazain:OnLoadBoth()
+    for i=1,6 do
+        local chainsSet = string.format("Chain%d", i)
+        AssignedPlayers[chainsSet] = {}
+    end
+    AZP.BossTools.Dormazain:CreateMainFrame()
+end
+
+function AZP.BossTools.Dormazain:OnLoadSelf()
+    C_ChatInfo.RegisterAddonMessagePrefix("AZPDORMINFO")
+
+    EventFrame = CreateFrame("FRAME", nil)
+    EventFrame:RegisterEvent("CHAT_MSG_ADDON")
+    EventFrame:RegisterEvent("GROUP_ROSTER_UPDATE")
+    EventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+    EventFrame:RegisterEvent("ENCOUNTER_END")
+    EventFrame:SetScript("OnEvent", function(...) AZP.BossTools.Dormazain:OnEvent(...) end)
+
+    AZPBTDormazainOptions = CreateFrame("FRAME", nil)
+    AZPBTDormazainOptions.name = "|cFF00FFFFDormazain|r"
+    AZPBTDormazainOptions.parent = AZP.BossTools.ParentOptionFrame.name
+    InterfaceOptions_AddCategory(AZPBTDormazainOptions)
+
+    AZPBTDormazainOptions.header = AZPBTDormazainOptions:CreateFontString("AZPBTDormazainOptions", "ARTWORK", "GameFontNormalHuge")
+    AZPBTDormazainOptions.header:SetPoint("TOP", 0, -10)
+    AZPBTDormazainOptions.header:SetText("|cFF00FFFFAzerPUG's BossTools Options!|r")
+    AZPBTDormazainOptions.SubHeader = AZPBTDormazainOptions:CreateFontString("AZPBTDormazainOptions", "ARTWORK", "GameFontNormalLarge")
+    AZPBTDormazainOptions.SubHeader:SetPoint("TOP", 0, -35)
+    AZPBTDormazainOptions.SubHeader:SetText("|cFF00FFFFDormazain|r")
+
+    AZPBTDormazainOptions.footer = AZPBTDormazainOptions:CreateFontString("AZPBTDormazainOptions", "ARTWORK", "GameFontNormalLarge")
+    AZPBTDormazainOptions.footer:SetPoint("TOP", 0, -400)
+    AZPBTDormazainOptions.footer:SetText(
+        "|cFF00FFFFAzerPUG Links:\n" ..
+        "Website: www.azerpug.com\n" ..
+        "Discord: www.azerpug.com/discord\n" ..
+        "Twitch: www.twitch.tv/azerpug\n|r"
+    )
+
+    AZP.BossTools.Dormazain:FillOptionsPanel(AZPBTDormazainOptions)
+    AZP.BossTools.Dormazain:OnLoadBoth()
+end
+
+function AZP.BossTools.Dormazain:CreateMainFrame()
+    local DormazainFrame = CreateFrame("FRAME", nil, UIPanel, "BackdropTemplate")
+    DormazainFrame:SetSize(300, 125)
+    DormazainFrame:SetPoint("TOPLEFT", 100, -200)
+    DormazainFrame:SetBackdrop({
+        bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+        edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+        edgeSize = 10,
+        insets = {left = 3, right = 3, top = 3, bottom = 3},
+    })
+    DormazainFrame:EnableMouse(true)
+    DormazainFrame:SetMovable(true)
+    DormazainFrame:RegisterForDrag("LeftButton")
+    DormazainFrame:SetScript("OnDragStart", DormazainFrame.StartMoving)
+    DormazainFrame:SetScript("OnDragStop", function() DormazainFrame:StopMovingOrSizing() end)
+
+    DormazainFrame.Header = DormazainFrame:CreateFontString("DormazainFrame", "ARTWORK", "GameFontNormalHuge")
+    DormazainFrame.Header:SetSize(DormazainFrame:GetWidth(), 25)
+    DormazainFrame.Header:SetPoint("TOP", 0, -5)
+    DormazainFrame.Header:SetText("Chains Assignments")
+
+    DormazainFrame.TextLabels = {}
+    DormazainFrame.LeftLabels = {}
+    DormazainFrame.MidLabels = {}
+    DormazainFrame.RightLabels = {}
+
+    for i = 1, 5 do
+        DormazainFrame.TextLabels[i] = DormazainFrame:CreateFontString("DormazainFrame", "ARTWORK", "GameFontNormal")
+        DormazainFrame.TextLabels[i]:SetSize(50, 25)
+        DormazainFrame.TextLabels[i]:SetPoint("TOPLEFT", 0, ((i - 1) * -15) -35)
+        DormazainFrame.TextLabels[i]:SetJustifyH("RIGHT")
+        DormazainFrame.TextLabels[i]:SetText(string.format("Chain %d:", i))
+
+        DormazainFrame.LeftLabels[i] = DormazainFrame:CreateFontString("DormazainFrame", "ARTWORK", "GameFontNormal")
+        DormazainFrame.LeftLabels[i]:SetSize(75, 25)
+        DormazainFrame.LeftLabels[i]:SetPoint("LEFT", DormazainFrame.TextLabels[i], "RIGHT", 5, 0)
+        DormazainFrame.LeftLabels[i]:SetJustifyH("MIDDLE")
+        DormazainFrame.LeftLabels[i]:SetText("-")
+
+        DormazainFrame.MidLabels[i] = DormazainFrame:CreateFontString("DormazainFrame", "ARTWORK", "GameFontNormal")
+        DormazainFrame.MidLabels[i]:SetSize(75, 25)
+        DormazainFrame.MidLabels[i]:SetPoint("LEFT", DormazainFrame.LeftLabels[i], "RIGHT", 5, 0)
+        DormazainFrame.MidLabels[i]:SetJustifyH("MIDDLE")
+        DormazainFrame.MidLabels[i]:SetText("-")
+
+        DormazainFrame.RightLabels[i] = DormazainFrame:CreateFontString("DormazainFrame", "ARTWORK", "GameFontNormal")
+        DormazainFrame.RightLabels[i]:SetSize(75, 25)
+        DormazainFrame.RightLabels[i]:SetPoint("LEFT", DormazainFrame.MidLabels[i], "RIGHT", 5, 0)
+        DormazainFrame.RightLabels[i]:SetJustifyH("MIDDLE")
+        DormazainFrame.RightLabels[i]:SetText("-")
+    end
+
+    DormazainFrame.closeButton = CreateFrame("Button", nil, DormazainFrame, "UIPanelCloseButton")
+    DormazainFrame.closeButton:SetSize(20, 21)
+    DormazainFrame.closeButton:SetPoint("TOPRIGHT", DormazainFrame, "TOPRIGHT", 2, 2)
+    DormazainFrame.closeButton:SetScript("OnClick", function() DormazainFrame:Hide() end)
+
+    AZP.BossTools.BossFrames.Dormazain = DormazainFrame
+    DormazainFrame:Hide()
+end
+
+function AZP.BossTools.Dormazain:FillOptionsPanel(frameToFill)
+    frameToFill.LockMoveButton = CreateFrame("Button", nil, frameToFill, "UIPanelButtonTemplate")
+    frameToFill.LockMoveButton:SetSize(100, 25)
+    frameToFill.LockMoveButton:SetPoint("TOPRIGHT", -75, -50)
+    frameToFill.LockMoveButton:SetText("Share List")
+    frameToFill.LockMoveButton:SetScript("OnClick", function() AZP.BossTools.Dormazain:ShareAssignees() end )
+
+    frameToFill.LockMoveButton = CreateFrame("Button", nil, frameToFill, "UIPanelButtonTemplate")
+    frameToFill.LockMoveButton:SetSize(100, 25)
+    frameToFill.LockMoveButton:SetPoint("TOPRIGHT", -75, -100)
+    frameToFill.LockMoveButton:SetText("Lock Frame")
+    frameToFill.LockMoveButton:SetScript("OnClick", function ()
+        if AZP.BossTools.BossFrames.Dormazain:IsMovable() then
+            AZP.BossTools.BossFrames.Dormazain:EnableMouse(false)
+            AZP.BossTools.BossFrames.Dormazain:SetMovable(false)
+            frameToFill.LockMoveButton:SetText("UnLock Frame!")
+        else
+            AZP.BossTools.BossFrames.Dormazain:EnableMouse(true)
+            AZP.BossTools.BossFrames.Dormazain:SetMovable(true)
+            frameToFill.LockMoveButton:SetText("Lock Frame")
+        end
+    end)
+
+    frameToFill.ShowHideButton = CreateFrame("Button", nil, frameToFill, "UIPanelButtonTemplate")
+    frameToFill.ShowHideButton:SetSize(100, 25)
+    frameToFill.ShowHideButton:SetPoint("TOPRIGHT", -75, -150)
+    frameToFill.ShowHideButton:SetText("Hide Frame!")
+    frameToFill.ShowHideButton:SetScript("OnClick", function () AZP.BossTools.Dormazain:ShowHideFrame() end)
+
+    frameToFill.textlabels = {}
+
+    frameToFill.LeftHeader = frameToFill:CreateFontString("LeftChainFrame", "ARTWORK", "GameFontNormalLarge")
+    frameToFill.LeftHeader:SetSize(100, 25)
+    frameToFill.LeftHeader:SetPoint("TOPLEFT", 70, -100)
+    frameToFill.LeftHeader:SetText("Left")
+
+    frameToFill.MidHeader = frameToFill:CreateFontString("LeftChainFrame", "ARTWORK", "GameFontNormalLarge")
+    frameToFill.MidHeader:SetSize(100, 25)
+    frameToFill.MidHeader:SetPoint("LEFT", frameToFill.LeftHeader, "RIGHT", 10, 0)
+    frameToFill.MidHeader:SetText("Mid")
+
+    frameToFill.RightHeader = frameToFill:CreateFontString("LeftChainFrame", "ARTWORK", "GameFontNormalLarge")
+    frameToFill.RightHeader:SetSize(100, 25)
+    frameToFill.RightHeader:SetPoint("LEFT", frameToFill.MidHeader, "RIGHT", 10, 0)
+    frameToFill.RightHeader:SetText("Right")
+
+    for i = 1,5 do
+        frameToFill.textlabels[i] = frameToFill:CreateFontString("frameToFill", "ARTWORK", "GameFontNormalLarge")
+        frameToFill.textlabels[i]:SetSize(50, 25)
+        frameToFill.textlabels[i]:SetPoint("TOPLEFT", 10, -30 * i - 100)
+        frameToFill.textlabels[i]:SetText(string.format("Chain %d:", i))
+
+        AZPBTDormazainLeftEditBoxes[i] = CreateFrame("EditBox", nil, frameToFill, "InputBoxTemplate")
+        AZPBTDormazainLeftEditBoxes[i]:SetSize(100, 25)
+        AZPBTDormazainLeftEditBoxes[i]:SetPoint("LEFT", frameToFill.textlabels[i], "RIGHT", 10, 0)
+        AZPBTDormazainLeftEditBoxes[i]:SetAutoFocus(false)
+        AZPBTDormazainLeftEditBoxes[i]:SetScript("OnEditFocusLost", function() AZP.BossTools.Dormazain:OnEditFocusLost("Left", i) end)
+
+        AZPBTDormazainMidEditBoxes[i] = CreateFrame("EditBox", nil, frameToFill, "InputBoxTemplate")
+        AZPBTDormazainMidEditBoxes[i]:SetSize(100, 25)
+        AZPBTDormazainMidEditBoxes[i]:SetPoint("LEFT", AZPBTDormazainLeftEditBoxes[i], "RIGHT", 10, 0)
+        AZPBTDormazainMidEditBoxes[i]:SetAutoFocus(false)
+        AZPBTDormazainMidEditBoxes[i]:SetScript("OnEditFocusLost", function() AZP.BossTools.Dormazain:OnEditFocusLost("Mid", i) end)
+
+        AZPBTDormazainRightEditBoxes[i] = CreateFrame("EditBox", nil, frameToFill, "InputBoxTemplate")
+        AZPBTDormazainRightEditBoxes[i]:SetSize(100, 25)
+        AZPBTDormazainRightEditBoxes[i]:SetPoint("LEFT", AZPBTDormazainMidEditBoxes[i], "RIGHT", 10, 0)
+        AZPBTDormazainRightEditBoxes[i]:SetAutoFocus(false)
+        AZPBTDormazainRightEditBoxes[i]:SetScript("OnEditFocusLost", function() AZP.BossTools.Dormazain:OnEditFocusLost("Right", i) end)
+    end
+
+    frameToFill:Hide()
+end
+
+function AZP.BossTools.Dormazain:OnEditFocusLost(position, chains)
+    local editBoxFrame = nil
+    local chainsSet = string.format("Chain%d", chains)
+    if position == "Left" then
+        editBoxFrame = AZPBTDormazainLeftEditBoxes[chains]
+    elseif position == "Mid" then
+        editBoxFrame = AZPBTDormazainMidEditBoxes[chains]
+    elseif position == "Right" then
+        editBoxFrame = AZPBTDormazainRightEditBoxes[chains]
+    end
+    if (editBoxFrame:GetText() ~= nil and editBoxFrame:GetText() ~= "") then
+        for k = 1, 40 do
+            local curName = GetRaidRosterInfo(k)
+            if curName ~= nil then
+                if string.find(curName, "-") then
+                    curName = string.match(curName, "(.+)-")
+                end
+                if curName == editBoxFrame:GetText() then
+                    local curGUID = UnitGUID("raid" .. k)
+                    AZPBTDormazainGUIDs[curGUID] = curName
+                    AssignedPlayers[chainsSet][position] = curGUID
+                    AZP.BossTools.Dormazain:UpdateMainFrame()
+                end
+            end
+        end
+    else
+        if AssignedPlayers[chainsSet] ~= nil then
+            AssignedPlayers[chainsSet][position] = nil
+        end
+    end
+
+    AZPBTDormazainChains = AssignedPlayers
+end
+
+function AZP.BossTools.Dormazain:ShareAssignees()
+    for ring, players in pairs(AssignedPlayers) do
+        if players ~= nil then
+            local message = string.format("%s:%s:%s:%s", ring, players.Left or "", players.Mid or "", players.Right or "")
+            C_ChatInfo.SendAddonMessage("AZPDORMINFO", message ,"RAID", 1)
+        end
+    end
+end
+
+
+function AZP.BossTools.Dormazain:CacheRaidNames()
+    if IsInRaid() == true then
+        for k = 1, 40 do
+            local curName = GetRaidRosterInfo(k)
+            if curName ~= nil then
+                if string.find(curName, "-") then
+                    curName = string.match(curName, "(.+)-")
+                end
+                local curGUID = UnitGUID("raid" .. k)
+                AZPBTDormazainGUIDs[curGUID] = curName
+            end
+        end
+    end
+end
+
+function AZP.BossTools.Dormazain:UpdateMainFrame()
+    if IsInRaid() == false then
+        print("BossTools Dormazain only works in raid.")
+        return
+    end
+    local playerGUID = UnitGUID("player")
+
+    for i = 1, 5 do
+        local chainsSet = string.format("Chain%d", i)
+        local ring = AssignedPlayers[chainsSet]
+        local left = ring.Left
+        local mid = ring.Mid
+        local right = ring.Right
+
+        if left ~= nil then
+            local name = AZPBTDormazainGUIDs[left]
+            if name == nil then name = "" end
+            AZP.BossTools.BossFrames.Dormazain.LeftLabels[i]:SetText(name)
+            AZPBTDormazainLeftEditBoxes[i]:SetText(name)
+        else
+            AZPBTDormazainLeftEditBoxes[i]:SetText("")
+            AZP.BossTools.BossFrames.Dormazain.LeftLabels[i]:SetText("")
+        end
+        if mid ~= nil then
+            local name = AZPBTDormazainGUIDs[mid]
+            if name == nil then name = "" end
+            AZP.BossTools.BossFrames.Dormazain.MidLabels[i]:SetText(name)
+            AZPBTDormazainMidEditBoxes[i]:SetText(name)
+        else
+            AZPBTDormazainMidEditBoxes[i]:SetText("")
+            AZP.BossTools.BossFrames.Dormazain.MidLabels[i]:SetText("")
+        end
+        if right ~= nil then
+            local name = AZPBTDormazainGUIDs[right]
+            if name == nil then name = "" end
+            AZP.BossTools.BossFrames.Dormazain.RightLabels[i]:SetText(name)
+            AZPBTDormazainRightEditBoxes[i]:SetText(name)
+        else
+            AZPBTDormazainRightEditBoxes[i]:SetText("")
+            AZP.BossTools.BossFrames.Dormazain.RightLabels[i]:SetText("")
+        end
+    end
+end
+
+function AZP.BossTools.Dormazain:ReceiveAssignees(receiveAssignees)     -- XXX
+    local chains, left, mid, right = string.match(receiveAssignees, "([^:]*):([^:]*):([^:]*):([^:]*)")
+    if left == "" then left = nil end
+    if mid == "" then mid = nil end
+    if right == "" then right = nil end
+    AssignedPlayers[chains] = {Left = left, Mid = mid, Right = right}
+    AZP.BossTools.Dormazain:UpdateMainFrame()
+end
+
+function AZP.BossTools.Dormazain:OnEvent(self, event, ...)
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+        AZP.BossTools.Dormazain.Events:CombatLogEventUnfiltered(...)
+    elseif event == "CHAT_MSG_ADDON" then
+        AZP.BossTools.Dormazain.Events:ChatMsgAddon(...)
+    elseif event == "ENCOUNTER_END" then
+        AZP.BossTools.Dormazain.Events:EncounterEnd(...)
+    end
+end
+
+function AZP.BossTools.Dormazain.Events:CombatLogEventUnfiltered(...)
+    local _, SubEvent, _, _, _, _, _, _, _, _, _, SpellID = CombatLogGetCurrentEventInfo()
+    if SubEvent == "SPELL_AURA_APPLIED" then
+        if SpellID == AZP.BossTools.IDs.Dormazain.Spell.WarmongerShackles then
+            AZP.BossTools.Dormazain.Events:WarmongerShackles()
+        end
+    end
+end
+
+function AZP.BossTools.Dormazain.Events:EncounterEnd(...)
+    ChainsCount = 0
+end
+
+function AZP.BossTools.Dormazain.Events:WarmongerShackles()
+    ChainsCount = ChainsCount + 1
+    local curChain = string.format("Chain%s", ChainsCount)
+    local curGUID = UnitGUID("PLAYER")
+    local assignedPosition = nil
+    if AZPBTDormazainChains[curChain].Left == curGUID then assignedPosition = "Left" end
+    if AZPBTDormazainChains[curChain].Mid == curGUID then assignedPosition = "Mid" end
+    if AZPBTDormazainChains[curChain].Right == curGUID then assignedPosition = "Right" end
+
+    if assignedPosition ~= nil then
+        local warnText = string.format("ChainSet %d - Pick up %s!", ChainsCount, assignedPosition)
+        AZP.BossTools:WarnPlayer(warnText)
+    end
+end
+
+function AZP.BossTools.Dormazain.Events:ChatMsgAddon(...)
+    local prefix, payload, _, sender = ...
+    if prefix == "AZPDORMINFO" then
+        AZP.BossTools.Dormazain:CacheRaidNames()
+        AZP.BossTools.Dormazain:ReceiveAssignees(payload)
+    end
+end
+
+AZP.BossTools.Dormazain:OnLoadSelf()
