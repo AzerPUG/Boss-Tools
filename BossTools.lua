9da52aa01bdca73e9c72ea76ebcc73e0dfdee263
Arco Dielhof <arcodielhof@hotmail.com>
2021-10-30 21:47:00 +0200

    Version++;

--- BossTools.lua
+++ BossTools.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["BossTools"] = 8
+AZP.VersionControl["BossTools"] = 9
 if AZP.BossTools == nil then AZP.BossTools = {} end
 if AZP.BossTools.Events == nil then AZP.BossTools.Events = {} end
 
