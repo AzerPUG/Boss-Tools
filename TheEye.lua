b45cfa0a0221893dfa33e5a11fb24af93cc933d7
Arco Dielhof <arcodielhof@hotmail.com>
2021-11-14 00:05:38 +0100

    Tested and fixed some bugs.

--- TheEye.lua
+++ TheEye.lua
@@ -10,23 +10,6 @@ local AZPBTTheEyeGUIDs, AZPBTTheEyeLeftEditBoxes, AZPBTTheEyeRightEditBoxes = {}
 
 local EventFrame = nil
 
-    --[[
-            ToDo List:
-            - Able to send all RaidIDs (RAID1, RAID2, RAID3)
-            - On receive, check the GUIDs and save those (we MUST save the GUIDs!!)
-            - On raidID change, make sure the GUIDs are still saved to the right location and the same people go in correcly
-            Popup what side to go to (Left and Right for now, marker addition later!)
-            
-
-
-
-
-            Not all names can be send at once. Split it to send only 1 name at a time?
-            Send GUID + Side and add to that specific sides list (AssignedPlayers.Left or AssignedPlayer.Right)
-            Create Option Panel, in the main frame should be a extra button, like with Dormazain!
-            ReCheck everything and test them shits.
-    ]]
-
 function AZP.BossTools.TheEye:OnLoadBoth()
     AZP.BossTools.TheEye:CreateMainFrame()
 end
@@ -53,7 +36,7 @@ function AZP.BossTools.TheEye:OnLoadSelf()
     AZPBTTheEyeOptions.SubHeader:SetText("|cFF00FFFFTheEye|r")
 
     AZPBTTheEyeOptions.footer = AZPBTTheEyeOptions:CreateFontString("AZPBTTheEyeOptions", "ARTWORK", "GameFontNormalLarge")
-    AZPBTTheEyeOptions.footer:SetPoint("TOP", 0, -400)
+    AZPBTTheEyeOptions.footer:SetPoint("TOP", 0, -450)
     AZPBTTheEyeOptions.footer:SetText(
         "|cFF00FFFFAzerPUG Links:\n" ..
         "Website: www.azerpug.com\n" ..
@@ -67,7 +50,7 @@ end
 
 function AZP.BossTools.TheEye:CreateMainFrame()
     local TheEyeFrame = CreateFrame("FRAME", nil, UIPanel, "BackdropTemplate")
-    TheEyeFrame:SetSize(300, 125)
+    TheEyeFrame:SetSize(350, 200)
     TheEyeFrame:SetPoint("TOPLEFT", 100, -200)
     TheEyeFrame:SetBackdrop({
         bgFile = "Interface/Tooltips/UI-Tooltip-Background",
@@ -84,28 +67,41 @@ function AZP.BossTools.TheEye:CreateMainFrame()
     TheEyeFrame.Header = TheEyeFrame:CreateFontString("TheEyeFrame", "ARTWORK", "GameFontNormalHuge")
     TheEyeFrame.Header:SetSize(TheEyeFrame:GetWidth(), 25)
     TheEyeFrame.Header:SetPoint("TOP", 0, -5)
-    TheEyeFrame.Header:SetText("sides Assignments")
+    TheEyeFrame.Header:SetText("Sides Assignments")
 
-    TheEyeFrame.TextLabels = {}
+    TheEyeFrame.Sepparator = CreateFrame("FRAME", nil, TheEyeFrame, "BackdropTemplate")
+    TheEyeFrame.Sepparator:SetSize(3, 150)
+    TheEyeFrame.Sepparator:SetPoint("BOTTOM", 0, 10)
+    TheEyeFrame.Sepparator:SetBackdrop({
+        bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+    })
+    TheEyeFrame.Sepparator:SetBackdropColor(0, 0, 0, 1)
+
     TheEyeFrame.LeftLabels = {}
     TheEyeFrame.RightLabels = {}
 
-    for i = 1, 20 do
-        TheEyeFrame.TextLabels[i] = TheEyeFrame:CreateFontString("TheEyeFrame", "ARTWORK", "GameFontNormal")
-        TheEyeFrame.TextLabels[i]:SetSize(50, 25)
-        TheEyeFrame.TextLabels[i]:SetPoint("TOPLEFT", 0, ((i - 1) * -15) -35)
-        TheEyeFrame.TextLabels[i]:SetJustifyH("RIGHT")
-        TheEyeFrame.TextLabels[i]:SetText(string.format("Chain %d:", i))
-
+    for i = 1, 10 do
         TheEyeFrame.LeftLabels[i] = TheEyeFrame:CreateFontString("TheEyeFrame", "ARTWORK", "GameFontNormal")
         TheEyeFrame.LeftLabels[i]:SetSize(75, 25)
-        TheEyeFrame.LeftLabels[i]:SetPoint("LEFT", TheEyeFrame.TextLabels[i], "RIGHT", 5, 0)
+        TheEyeFrame.LeftLabels[i]:SetPoint("TOPLEFT", 0, ((i - 1) * -15) -35)
         TheEyeFrame.LeftLabels[i]:SetJustifyH("MIDDLE")
         TheEyeFrame.LeftLabels[i]:SetText("-")
 
+        TheEyeFrame.LeftLabels[i + 10] = TheEyeFrame:CreateFontString("TheEyeFrame", "ARTWORK", "GameFontNormal")
+        TheEyeFrame.LeftLabels[i + 10]:SetSize(75, 25)
+        TheEyeFrame.LeftLabels[i + 10]:SetPoint("LEFT", TheEyeFrame.LeftLabels[i], "RIGHT", 5, 0)
+        TheEyeFrame.LeftLabels[i + 10]:SetJustifyH("MIDDLE")
+        TheEyeFrame.LeftLabels[i + 10]:SetText("-")
+
+        TheEyeFrame.RightLabels[i + 10] = TheEyeFrame:CreateFontString("TheEyeFrame", "ARTWORK", "GameFontNormal")
+        TheEyeFrame.RightLabels[i + 10]:SetSize(75, 25)
+        TheEyeFrame.RightLabels[i + 10]:SetPoint("TOPRIGHT", 0, ((i - 1) * -15) -35)
+        TheEyeFrame.RightLabels[i + 10]:SetJustifyH("MIDDLE")
+        TheEyeFrame.RightLabels[i + 10]:SetText("-")
+
         TheEyeFrame.RightLabels[i] = TheEyeFrame:CreateFontString("TheEyeFrame", "ARTWORK", "GameFontNormal")
         TheEyeFrame.RightLabels[i]:SetSize(75, 25)
-        TheEyeFrame.RightLabels[i]:SetPoint("LEFT", TheEyeFrame.LeftLabels[i], "RIGHT", 5, 0)
+        TheEyeFrame.RightLabels[i]:SetPoint("RIGHT", TheEyeFrame.RightLabels[i + 10], "LEFT", -5, 0)
         TheEyeFrame.RightLabels[i]:SetJustifyH("MIDDLE")
         TheEyeFrame.RightLabels[i]:SetText("-")
     end
@@ -244,29 +240,20 @@ function AZP.BossTools.TheEye:ShareAssignees()
     end
 end
 
-
-function AZP.BossTools.TheEye:ReceiveAssignees(receiveAssignees)     -- XXX
-    print(receiveAssignees)
+function AZP.BossTools.TheEye:ReceiveAssignees(receiveAssignees)
     local left, right = string.match(receiveAssignees, "([^:]*):([^:]*)")
     AssignedPlayers.Left = {}
     AssignedPlayers.Right = {}
     for RaidID in string.gmatch(left, "%d+") do
-        print("Raid ID left: ", RaidID)
         table.insert(AssignedPlayers.Left, UnitGUID(string.format("raid%d", RaidID)))
     end
 
     for RaidID in string.gmatch(right, "%d+") do
-        print("Raid ID right: ", RaidID)
         table.insert(AssignedPlayers.Right, UnitGUID(string.format("raid%d", RaidID)))
     end
 
-    -- local sides, left, right = string.match(receiveAssignees, "([^:]*):([^:]*):([^:]*)")
-    -- if left == "" then left = nil end
-    -- if right == "" then right = nil end
-    -- AssignedPlayers = {Left = left, Right = right}
     AZP.BossTools.TheEye:UpdateMainFrame()
 end
-
 
 function AZP.BossTools.TheEye:CacheRaidNames()
     if IsInRaid() == true then
@@ -316,7 +303,6 @@ function AZP.BossTools.TheEye:UpdateMainFrame()
     end
 end
 
-
 function AZP.BossTools.TheEye:OnEvent(self, event, ...)
     if event == "COMBAT_LOG_EVENT_UNFILTERED" then
         AZP.BossTools.TheEye.Events:CombatLogEventUnfiltered(...)
@@ -344,10 +330,6 @@ function AZP.BossTools.TheEye.Events:StygianEjection()
     elseif tContains(AZPBTTheEyeSides.Right, curGUID) then
         side = "Right"
     end
-    -- for i = 1, #AZPBTTheEyeSides do
-    --     if AZPBTTheEyeChains.Left[i] == curGUID then side = "Left" end
-    --     if AZPBTTheEyeChains.Right[i] == curGUID then side = "Right" end
-    -- end
 
     if side ~= nil then
         local warnText = string.format("SplitPhase! Go %s!", side)  -- ("SplitPhase! Go %s%S%s!", marker, side, marker)
