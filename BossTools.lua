c6b4b34ff81b7782cfba7a13830929811720344c
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2022-02-04 20:36:17 +0100

    Version++;

--- BossTools.lua
+++ BossTools.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["BossTools"] = 15
+AZP.VersionControl["BossTools"] = 16
 if AZP.BossTools == nil then AZP.BossTools = {} end
 if AZP.BossTools.Events == nil then AZP.BossTools.Events = {} end
 
